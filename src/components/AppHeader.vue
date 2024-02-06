5de1424cd779110697b730577ca5a797e370795e
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-02-06 17:03:43 +0100

    header_and_background

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: "AppHeader",
+    data: () => ({
+        imgName: "pokemon_header.jpg"
+    }),
+    methods: {
+        createImagePath(imgName) {
+            const url = new URL(`../assets/imgs/${imgName}`, import.meta.url);
+            return url.href
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <figure>
+            <img :src="createImagePath(imgName)" :alt="imgName" :title="imgName">
+        </figure>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+
+    figure {
+        height: 200px;
+
+        img {
+            margin: 0 auto;
+            max-height: 200px;
+        }
+    }
+
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "AppMain"
+}
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <div class="pokedex-screen"></div>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+    height: calc(100vh - 200px);
+    background-image: url("../assets/imgs/pokemon_bg.jpg");
+}
+</style>
